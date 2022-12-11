902c161ec67b01ca15e37f20fb3ab452a025b2f8
Kubilay BAYINDIR <45577585+bayindirkubilay60@users.noreply.github.com>
2022-12-11 13:27:49 +0300

    Templated was added inside of project.Dasboard page was created

--- SupperCRMApplication.WebApp/Controllers/HomeController.cs
+++ SupperCRMApplication.WebApp/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace SupperCRMApplication.WebApp.Controllers
         {
             return View();
         }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
